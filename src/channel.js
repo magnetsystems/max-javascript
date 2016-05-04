3559ec6cede09228f0aecdb2870b70fc0888ad43
Edward Yang <edward.yang@magnet.com>
2016-05-04 10:14:33 -0700

    fix integer type channel name bug

--- src/channel.js
+++ src/channel.js
@@ -1186,11 +1186,11 @@ Max.Channel.prototype.getChannelName = function() {
  * @ignore
  */
 Max.Channel.prototype.getChannelId = function() {
-    return (this.isPublic === true ? this.name : (this.userId + '#' + this.name)).toLowerCase();
+    return (this.isPublic === true ? (this.name+'') : (this.userId + '#' + this.name)).toLowerCase();
 };
 
 Max.Channel.prototype.getNodePath = function() {
-    return '/' + Max.App.appId + '/' + (this.userId ? this.userId : '*') + '/' + this.name.toLowerCase();
+    return ('/' + Max.App.appId + '/' + (this.userId ? this.userId : '*') + '/' + (this.name+'')).toLowerCase();
 };
 
 // non-persistent cache of channel information to improve message receive performance
